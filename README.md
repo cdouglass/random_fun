23de8452bdee0a2db854f77c90450ca098842360
Cathy Douglass <cdouglass256@gmail.com>
2017-09-13 15:17:48 -0700

    Get actual seed from random.org and update README

--- README.md
+++ README.md
@@ -12,10 +12,18 @@ When you run
 ```
 $ cargo run
 ```
-a bitmap file `hello.ppm` format will be created in your current directory.
+a bitmap file named `hello.ppm` will be created in your current directory.
 
 ##General information
 
 The image is populated by a manually seeded PRNG that uses the ISAAC algorithm. This is *probably* cryptographically secure, but don't bet your life on it. From the `rand` crate [documentation](https://docs.rs/rand/0.3.16/rand/isaac/struct.IsaacRng.html):
 
 > The ISAAC algorithm is generally accepted as suitable for cryptographic purposes, but this implementation has not be verified as such. Prefer a generator like OsRng that defers to the operating system for cases that need high security.
+
+The seed consists of 16 bytes (128 bits of randomness) obtained from the random.org HTTP API.
+
+##Not implemented (yet)
+* Any sort of error handling at all
+* Checking random.org quota status (but as this uses a CSPRNG to use entropy efficiently, we're not likely to hit the quota with reasonable usage)
+* Including user's email address in user agent (good manners, so that if I made a mistake and the program misbehaves, random.org can let me know)
+* CLI flags to choose file name, image size, or amount of entropy to require
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+extern crate hyper;
+extern crate hyper_native_tls;
+use hyper::Client;
+use hyper::net::HttpsConnector;
+use hyper_native_tls::NativeTlsClient;
+
 extern crate rand;
-use rand::Rng;
-use rand::SeedableRng;
-use rand::StdRng;
+use rand::{Rng, SeedableRng, StdRng};
 
 use std::fs::File;
 use std::io::prelude::*;
@@ -10,11 +14,42 @@ fn main() {
     let filename = "hello.ppm";
     let size = 128;
     let header = format!("P6\n{} {}\n255\n", size, size);
-    let seed: &[_] = &[1, 2, 3, 4];
-    let mut rng = StdRng::from_seed(seed);
-    let mut bytes: Vec<u8> = Vec::with_capacity(size * size);
+
+    let ssl = NativeTlsClient::new().unwrap();
+    let connector = HttpsConnector::new(ssl);
+    let client = Client::with_connector(connector);
+    // get 16x 8-bit integers, for 128 bits of randomness in total
+    let url = "https://www.random.org/sequences/?num=128&min=0&max=255&col=1&format=plain&rnd=new";
+    let mut response = client.get(url).send().unwrap();
+    let mut seed_string = String::new();
+    // fills string with numbers, separated by newlines
+    response.read_to_string(&mut seed_string).unwrap();
+    let seed = string_to_bytes(seed_string);
+
+    // wants to be seeded with usize
+    let mut rng = StdRng::from_seed(seed.as_slice());
+    // w * h * 3
+    let mut bytes = [0u8; 49152];
+
     rng.fill_bytes(&mut bytes);
     let mut file = File::create(filename).unwrap();
     file.write_all(header.as_bytes()).unwrap();
     file.write_all(&bytes).unwrap();
+}
+
+fn string_to_bytes(s: String) -> Vec<usize> {
+    let mut bytes = vec![];
+
+    let mut cur = String::new();
+    for c in s.chars() {
+        if c.is_numeric() {
+            cur.push(c);
+        } else {
+            let byte = cur.parse::<usize>().unwrap();
+            bytes.push(byte);
+            cur = String::new();
+        }
+    }
+
+    bytes
 }
